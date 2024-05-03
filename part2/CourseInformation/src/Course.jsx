8cbf1014e85d7e2159a90038f14380a4b60e14dd
Artur Anikeich <65artur65@gmail.com>
2024-05-03 15:13:30 +0100

    2.4: Course information step9 and 2.5: Separate module step10

--- part2/CourseInformation/src/Course.jsx
+++ part2/CourseInformation/src/Course.jsx
@@ -1,6 +1,6 @@
 const Header = ({props}) => {
     console.log("Header args", props)
-    return <h1>{props.name}</h1>
+    return <h2>{props.name}</h2>
 }
 
 const Part = ({props}) => {
@@ -11,24 +11,34 @@ const Part = ({props}) => {
         </p>
     )
 }
+
+const Total = ({props}) => {
+    return <h3>total of { props.reduce((sum, i) => sum + i.exercises, 0) } exercises</h3>
+}
   
 const Content = ({props}) => {
     const array = props.parts
     console.log("Content array", array)
     return (
         <div>
-            { array.map(i => <Part key={i.id} props={i} />) }
-            <h3>total of { array.reduce((sum, i) => sum + i.exercises, 0) } exercises</h3>
+            { array.map(i => <Part key={i.id + props.id} props={i} />) }
+            <Total props={array} />
         </div>
     )
 }
 
-const Course = ({course}) => {
-    console.log("Course args", course)
+const Course = ({courses}) => {
+    console.log("Course args", courses)
     return (
         <div>
-            <Header props={course} />
-            <Content props={course} />
+            <h1>Web development curriculum</h1>
+            { courses.map(i => 
+                <div key={i.id}>
+                    {/*console.log("id", i.id)*/}
+                    <Header key={i.id + 100} props={i} />
+                    <Content key={i.id + 1000} props={i} />
+                </div>
+            )}
         </div>
     )
 }
