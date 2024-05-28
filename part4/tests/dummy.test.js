1cb020975aa52b7b559e7174fafc5f449206d84f
Artur Anikeich <65artur65@gmail.com>
2024-05-28 20:59:34 +0100

    4.4: Helper Functions and Unit Tests, step 2

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -1,10 +1,28 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const blogs =  require('./dummy_list')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    test('when list has no blogs, equals to 0', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes([blogs[0]])
+    assert.strictEqual(result, 7)
+  })
+
+  test('sum of likes of several blogs', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 36)
+  })
 })
