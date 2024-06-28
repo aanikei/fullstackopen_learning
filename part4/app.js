aa70307c2e1f2de54f51631ae88cfc4547993d0e
Artur Anikeich <65artur65@gmail.com>
2024-06-28 12:03:15 +0100

    4.20*: Blog List Expansion, step 8

--- part4/app.js
+++ part4/app.js
@@ -3,13 +3,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const middleware = require('./utils/middleware')
+
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+
+app.use(middleware.errorHandler)
+app.use(middleware.tokenExtractor)
 
 app.use(cors())
 app.use(express.json())
@@ -17,7 +21,5 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-
-app.use(middleware.errorHandler)
 
 module.exports = app
