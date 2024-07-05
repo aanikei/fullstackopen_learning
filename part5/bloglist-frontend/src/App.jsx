79663691952210357230d534aa6ff8cd46801dbb
Artur Anikeich <65artur65@gmail.com>
2024-07-05 20:40:48 +0100

    5.2: Blog List Frontend, step 2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -15,6 +15,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -23,6 +31,7 @@ const App = () => {
         username, password
       })
       
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
       setUser(user)
       console.log("user.name", user.name)
       setUsername('')
@@ -30,6 +39,11 @@ const App = () => {
     } catch (e) {
       alert('wrong credentials')
     }
+  }
+
+  const handleLogout = async () => {
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -70,7 +84,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout} type="submit">logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
