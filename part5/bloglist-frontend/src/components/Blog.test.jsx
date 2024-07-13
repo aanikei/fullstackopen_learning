0b8dd7b6ccb3aba4b8bb99f908969a2bb69c60b6
Artur Anikeich <65artur65@gmail.com>
2024-07-13 11:09:24 +0100

    5.15: Blog List Tests, step 3

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -69,7 +69,7 @@ describe('<Blog />', () => {
     const button = screen.getByText('view')
     await loggedUser.click(button)
 
-    screen.debug()
+    //screen.debug()
 
     const element = screen.getByText(`${blog.title} ${blog.author}`)
     expect(element).toBeDefined()
@@ -79,5 +79,42 @@ describe('<Blog />', () => {
 
     const url = screen.queryByText(`${blog.url}`)
     expect(url).toBeDefined()
+  })
+
+  test('like button functionality', async () => {
+    const blog = {
+      'title': 'Test Blog',
+      'author': 'Test Author',
+      'url': 'https://abc.xyz',
+      'likes': 0,
+      'id': '667d801e70ca74add7a8724d',
+      'user': '667d801e70ca74add7a8724c'
+    }
+
+    const user = {
+      'username': 'testuser23',
+      'name': 'User 23',
+      'id': '667d801e70ca74add7a8724c'
+    }
+
+    const mockHandler = vi.fn()
+
+    render(<Blog
+      key={blog.id}
+      blog={blog}
+      addLikes={mockHandler}
+      //removeBlog={removeBlog}
+      user={user}
+    />)
+
+    const loggedUser = userEvent.setup()
+    const buttonView = screen.getByText('view')
+    await loggedUser.click(buttonView)
+
+    const buttonLike = screen.getByText('like')
+    await loggedUser.click(buttonLike)
+    await loggedUser.click(buttonLike)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
